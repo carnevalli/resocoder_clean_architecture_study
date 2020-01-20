476e611e35a6aeb08963bbb6ecddd2bf3b732033
Tadeu Carnevalli <tcarnevalli@gmail.com>
2020-01-20 19:11:56 -0300

    model created. end of chapter 4

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:resocoder_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import "package:meta/meta.dart";
+
+class NumberTriviaModel extends NumberTrivia {
+
+  NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'], 
+      // The 'num' type can be both a 'double' and an 'int'
+      number: (json['number'] as num).toInt()
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+}
